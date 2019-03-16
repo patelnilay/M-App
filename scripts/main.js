f00646c224e46bd46600ed95613e438f991985f9
Nilay Patel <nilay.patel@ada.ac.uk>
2019-03-16 10:12:53 +0000

    created a place marker on Google King Cross

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,19 @@
+// initialises map. Gets called in API script tag
 function initMap() {
   // options for map
   var options = {
     zoom: 10,
     center: {
-      lat: 51.5074, lng: -0.1278},
-  }
+      lat: 51.5074,lng: -0.1278},
+  };
 
   // new map
   var map = new google.maps.Map(document.getElementById('map'), options);
 
-}
+  // add marker to map at Google Kings Cross
+  var placeMarker = new google.maps.Marker({
+    position: {
+      lat: 51.53331, lng: -0.1260625},
+    map: map
+  });
+};
