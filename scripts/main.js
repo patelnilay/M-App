ea9822816eb366ed2969382bc8c49bca7ad8c0a8
Nilay Patel <nilay.patel@ada.ac.uk>
2019-03-17 17:37:44 +0000

    added geolocation functionality and mapped it to a button created in the index file. When the button is pressed the browser will now ask the user for permission to get their current location.

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,15 @@
 // initialises map. Gets called in API script tag
 function initMap() {
+
+  var infoWindow
+
   // options for map
   var options = {
     zoom: 10,
     center: {
-      lat: 51.5074,lng: -0.1278},
+      lat: 51.5074,
+      lng: -0.1278
+    },
   };
 
   // new map
@@ -13,7 +18,9 @@ function initMap() {
   // add marker to map at Google Kings Cross
   var placeMarker = new google.maps.Marker({
     position: {
-      lat: 51.53331, lng: -0.1260625},
+      lat: 51.53331,
+      lng: -0.1260625
+    },
     map: map
   });
 
@@ -21,7 +28,49 @@ function initMap() {
     content: '<h1> Google Kings Cross </h2>',
   });
 
-  placeMarker.addListener('click',function(){
+  placeMarker.addListener('click', function() {
     informationWindow.open(map, placeMarker);
   });
-};
+
+  let button = document.getElementById("get-location");
+  console.log(button)
+
+  button.onclick= function() {
+    geoLocate(map, infoWindow);
+  };
+
+}
+
+function geoLocate(map, infoWindow) {
+
+  var infoWindow = new google.maps.InfoWindow;
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+      'Error: The Geolocation service failed.' :
+      'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+  }
+
+}
